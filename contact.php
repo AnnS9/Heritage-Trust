a7d7da2249cc93c3377315c6780820854683c9f6
AnnS9 <75726606+AnnS9@users.noreply.github.com>
2022-05-10 22:35:58 +0100

    bk

--- contact.php
+++ contact.php
@@ -74,3 +74,19 @@ if (isset($_POST['Email'])) {
 <?php
 }
 ?>
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("your@email.address", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
